47e9591a2f51b08949143debf98e27538a29c03d
hyperneutr0n <randylemuel21@gmail.com>
2025-09-20 13:37:05 +0700

    moved file server inside /app

--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 )
 
 func main() {
-	const filePathRoot = "."
+	const filePathRoot = "app"
 	const port = "8080"
 
 	mux := http.NewServeMux()
 	
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	fs := http.FileServer(http.Dir(filePathRoot))
+	mux.Handle("/app/", http.StripPrefix("/app/", fs))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		if _, err := w.Write([]byte("OK")); err != nil {
+			log.Printf("Error writing response: %v", err)
+		}
+	})
 
 	server := &http.Server{
 		Addr: ":" + port,
