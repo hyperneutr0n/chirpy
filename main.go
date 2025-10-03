787363a3c84e089a0b4b1c4572d46c75a367b579
hyperneutr0n <randylemuel21@gmail.com>
2025-10-03 15:30:18 +0700

    now every /app hit is being counted

--- main.go
+++ main.go
@@ -3,30 +3,69 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fsHit atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fsHit.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	count := cfg.fsHit.Load()
+
+	s := "Hits: " + strconv.FormatInt(int64(count), 10)
+	data := []byte(s)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(data))
+	if err != nil {
+		log.Printf("error writing response: %v", err)
+	}
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fsHit.Store(0)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	const filePathRoot = "app"
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	
-	fs := http.FileServer(http.Dir(filePathRoot))
-	mux.Handle("/app/", http.StripPrefix("/app/", fs))
+	apiCfg := apiConfig{fsHit: atomic.Int32{}}
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		if _, err := w.Write([]byte("OK")); err != nil {
-			log.Printf("Error writing response: %v", err)
-		}
-	})
+	fs := http.FileServer(http.Dir(filePathRoot))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
+
+	mux.HandleFunc("/healthz", handlerHealthz)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
 	log.Printf("Starting server on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write([]byte(http.StatusText(http.StatusOK))); err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
 }
