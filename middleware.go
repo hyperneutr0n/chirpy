22443db1526ca87f290aacd6f9fbcda735169665
hyperneutr0n <randylemuel21@gmail.com>
2026-03-05 11:50:40 +0700

    Test

--- middleware.go
+++ middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fsHit.Add(1)
+		cfg.fsHit.Add(2)
 		next.ServeHTTP(w, r)
 	})
 }
